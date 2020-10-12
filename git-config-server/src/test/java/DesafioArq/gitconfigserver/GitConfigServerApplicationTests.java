fe604ba723e957472c47bf3de657251df82a6f55
Rodolfo <rodolfoalmeida_a@hotmail.com>
2020-10-11 23:58:16 -0300

    commit

---
+++ git-config-server/src/test/java/DesafioArq/gitconfigserver/GitConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package DesafioArq.gitconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
